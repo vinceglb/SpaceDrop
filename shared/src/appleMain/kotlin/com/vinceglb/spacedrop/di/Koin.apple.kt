5aa1d864f8802d43d86166ba7b08501fa8aa323c
Vincent Guillebaud <vincent.guillebaud31@gmail.com>
2023-12-10 02:03:30 +0100

    🏗️ Add platformModule for Koin method

--- shared/src/appleMain/kotlin/com/vinceglb/spacedrop/di/Koin.apple.kt
+++ shared/src/appleMain/kotlin/com/vinceglb/spacedrop/di/Koin.apple.kt
@@ -1,8 +1,16 @@
 package com.vinceglb.spacedrop.di
 
+import com.vinceglb.spacedrop.data.settings.SettingsFactory
 import io.github.jan.supabase.gotrue.AuthConfig
+import org.koin.core.module.Module
+import org.koin.core.module.dsl.factoryOf
+import org.koin.dsl.module
 
 actual fun AuthConfig.platformAuthConfig() {
     host = "com.vinceglb.spacedrop"
     scheme = "login"
+}
+
+actual fun platformModule(): Module = module {
+    factoryOf(::SettingsFactory)
 }
