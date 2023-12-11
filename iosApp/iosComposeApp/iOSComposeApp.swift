1b987705b064091fb34f91bdddf34fc98ffbffcb
Vincent Guillebaud <vincent.guillebaud31@gmail.com>
2023-12-11 19:05:48 +0100

    ➕ Add Firebase Analytics and Messaging to SwiftPM

--- iosApp/iosComposeApp/iOSComposeApp.swift
+++ iosApp/iosComposeApp/iOSComposeApp.swift
@@ -7,10 +7,20 @@
 //
 
 import SwiftUI
+import FirebaseCore
 import ComposeApp
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
 
 @main
 struct iOSComposeApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     init() {
         MainViewControllerKt.doInitKoinIOS()
     }
