883931a10cd7f8d36a3cb3e0abffcde3a690a7ec
Vincent Guillebaud <vincent.guillebaud31@gmail.com>
2023-12-13 13:20:16 +0100

    ✨ Setup FCM on iOS

--- iosApp/iosComposeApp/iOSComposeApp.swift
+++ iosApp/iosComposeApp/iOSComposeApp.swift
@@ -8,12 +8,62 @@
 
 import SwiftUI
 import FirebaseCore
+import FirebaseMessaging
 import ComposeApp
 
-class AppDelegate: NSObject, UIApplicationDelegate {
+class AppDelegate: NSObject, UIApplicationDelegate, UNUserNotificationCenterDelegate, MessagingDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        // Connect to APNs
+        application.registerForRemoteNotifications()
+        
+        // Firebase
         FirebaseApp.configure()
+        
+        // Delegate
+        Messaging.messaging().delegate = self
+        UNUserNotificationCenter.current().delegate = self
+        
+        // Ask Notifications permission
+        let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+        UNUserNotificationCenter.current().requestAuthorization(
+          options: authOptions,
+          completionHandler: { _, _ in }
+        )
+        
         return true
+    }
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        // Link APNs token to Firebase
+        Messaging.messaging().apnsToken = deviceToken
+    }
+    
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        // Retrieve FCM token
+        messaging.token { token, error in
+            if let error = error {
+                print("Error fetching FCM registration token: \(error)")
+            } else if let token = token {
+                print("FCM registration token: \(token)")
+            }
+        }
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse) async {
+        let userInfo = response.notification.request.content.userInfo
+        print("userNotificationCenter - Receive notification \(userInfo)")
+        
+        // Tel Firebase that we receive the message
+        Messaging.messaging().appDidReceiveMessage(userInfo)
+    }
+    
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any]) async -> UIBackgroundFetchResult {
+        print("application - Receive notification \(userInfo)")
+        
+        // Tel Firebase that we receive the message
+        Messaging.messaging().appDidReceiveMessage(userInfo)
+        
+        return UIBackgroundFetchResult.newData
     }
 }
 
@@ -24,14 +74,14 @@ struct iOSComposeApp: App {
     init() {
         MainViewControllerKt.doInitKoinIOS()
     }
-
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .onOpenURL(perform: handleDeepLink)
         }
     }
-
+    
     func handleDeepLink(_ url: URL) {
         if url.scheme == "login" {
             SupabaseUtil.shared.handleDeeplinks(url: url)
