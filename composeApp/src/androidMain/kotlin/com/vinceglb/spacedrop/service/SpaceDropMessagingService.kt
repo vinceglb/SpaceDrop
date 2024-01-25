53e41d07a86671af77436f6f399fc96afed47cae
Vincent Guillebaud <vincent.guillebaud31@gmail.com>
2024-01-25 20:39:01 +0100

    ⚗️ Experiment with notifications

--- composeApp/src/androidMain/kotlin/com/vinceglb/spacedrop/service/SpaceDropMessagingService.kt
+++ composeApp/src/androidMain/kotlin/com/vinceglb/spacedrop/service/SpaceDropMessagingService.kt
@@ -4,11 +4,13 @@ import co.touchlab.kermit.Logger
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import com.vinceglb.spacedrop.data.repository.DeviceRepository
+import com.vinceglb.spacedrop.data.repository.EventRepository
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 import org.koin.android.ext.android.inject
 
 class SpaceDropMessagingService : FirebaseMessagingService() {
+    private val eventRepository: EventRepository by inject()
     private val deviceRepository: DeviceRepository by inject()
     private val applicationScope: CoroutineScope by inject()
 
@@ -22,6 +24,15 @@ class SpaceDropMessagingService : FirebaseMessagingService() {
 
     override fun onMessageReceived(message: RemoteMessage) {
         Logger.i(TAG) { "Message received from: ${message.from}" }
+
+        // Check if message contains a data payload.
+        message.data.isNotEmpty().let {
+            val id = message.data["id"] ?: ""
+            applicationScope.launch {
+                Logger.i(TAG) { "Message data payload: $id / ${message.data}" }
+                eventRepository.executeEvents()
+            }
+        }
     }
 
     companion object {
