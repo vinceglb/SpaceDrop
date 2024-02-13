6900b677c867974d092c6a26b950c9f56756730c
Vincent Guillebaud <vincent.guillebaud31@gmail.com>
2024-02-13 20:51:48 +0100

    ✨ Add copyToClipboard and openUrl to iOS

--- shared/src/appleMain/kotlin/com/vinceglb/spacedrop/data/platform/PlatformActionDataSource.apple.kt
+++ shared/src/appleMain/kotlin/com/vinceglb/spacedrop/data/platform/PlatformActionDataSource.apple.kt
@@ -1,10 +1,12 @@
 package com.vinceglb.spacedrop.data.platform
 
+import platform.Foundation.NSURL
 import platform.UIKit.UIAlertAction
 import platform.UIKit.UIAlertActionStyleDefault
 import platform.UIKit.UIAlertController
 import platform.UIKit.UIAlertControllerStyleAlert
 import platform.UIKit.UIApplication
+import platform.UIKit.UIPasteboard
 
 actual class PlatformActionDataSource {
     actual suspend fun sendNotification() {
@@ -27,5 +29,16 @@ actual class PlatformActionDataSource {
             animated = true,
             completion = null
         )
+    }
+
+    actual fun copyToClipboard(text: String) {
+        val pasteboard = UIPasteboard.generalPasteboard
+        pasteboard.string = text
+    }
+
+    actual fun openUrl(url: String) {
+        NSURL.URLWithString(url)?.let {
+            UIApplication.sharedApplication.openURL(it)
+        }
     }
 }
